78b76a483a50b612a40fb7cf16254b49e35acf67
timedout <git@nexy7574.co.uk>
2025-10-28 00:57:54 +0000

    update remotes again

--- setup-git.sh
+++ setup-git.sh
@@ -5,7 +5,7 @@ set -e
 OTHER_REMOTES=("ssh://git@git.gay/nex/proof.git" "ssh://git@git.nexy7574.co.uk:222/nex/proof.git" "ssh://git@github.com/nexy7574/proof.git")
 
 git remote remove origin || true
-git remote add origin git@codeberg.org:timedout/proof.git
+git remote add origin ssh://git@codeberg.org/timedout/proof.git
 
 for url in "${OTHER_REMOTES[@]}"; do
   echo "Adding remote: $url"
